df9143f82eb6754e86f3a03a05c1d07a14d6c7c3
PaulTonData <paul.ton@outlook.com>
2021-11-21 01:55:40 +0000

    add media_root for serving from apache2

--- easylife/settings.py
+++ easylife/settings.py
@@ -120,10 +120,12 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+#STATIC_ROOT = '/opt/bitnami/projects/easylife/static/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/posting_images/'
-MEDIA_ROOT = ''
+MEDIA_ROOT = '/opt/bitnami/projects/easylife/'
