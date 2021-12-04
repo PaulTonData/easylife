91e1a2e62ba807adb986cb5abeb6d959e2c23826
Paul Ton <paulton@Pauls-MBP.fios-router.home>
2021-12-04 17:28:37 -0500

    capture image thumbnail on upload and display thumbnails in browse listings view

--- marketplace/models.py
+++ marketplace/models.py
@@ -1,6 +1,13 @@
+import os.path
+
 from django.contrib.auth.models import User
+from django.core.files.base import ContentFile
 from django.db import models
 from django.template.defaultfilters import slugify
+from io import BytesIO
+from PIL import Image
+
+THUMB_SIZE = (200, 200)
 
 class Posting(models.Model):
     title = models.CharField(max_length=200)
@@ -21,11 +28,52 @@ def get_image_filename(instance, filename):
     slug = slugify(title)
     return "posting_images/%s-%s" % (slug, filename)
 
+def get_thumbnail_filename(instance, filename):
+    title = instance.posting.title
+    slug = slugify(title)
+    return "posting_thumbnails/%s-%s" % (slug, filename)
+
 class Photo(models.Model):
     posting = models.ForeignKey(Posting, on_delete=models.CASCADE)
     alt_text = models.CharField(max_length=200, blank=True)
     image = models.ImageField(upload_to=get_image_filename,
                               verbose_name="Image", blank=True)
+    thumbnail = models.ImageField(upload_to=get_thumbnail_filename,
+                             verbose_name="Thumbnail", blank=True)
+
+    def save(self, *args, **kwargs):
+        if not self.make_thumbnail():
+            # set to default thumbnail
+            raise Exception("Could not create thumbnail - is the file type valid?")
+
+        super(Photo, self).save(*args, **kwargs)
+
+    def make_thumbnail(self):
+        image = Image.open(self.image)
+        image.thumbnail(THUMB_SIZE, Image.ANTIALIAS)
+
+        thumb_name, thumb_extension = os.path.splitext(self.image.name)
+        thumb_extension = thumb_extension.lower()
+
+        thumb_filename = thumb_name + '_thumb' + thumb_extension
+
+        if thumb_extension in ['.jpg', '.jpeg']:
+            FTYPE = 'JPEG'
+        elif thumb_extension == '.gif':
+            FTYPE = 'GIF'
+        elif thumb_extension == '.png':
+            FTYPE = 'PNG'
+        else:
+            return False # Unrecognized file type
+    
+        temp_thumb = BytesIO()
+        image.save(temp_thumb, FTYPE)
+        temp_thumb.seek(0)
+
+        self.thumbnail.save(thumb_filename, ContentFile(temp_thumb.read()), save=False)
+        temp_thumb.close()
+
+        return True
 
     def __str__(self):
         return self.alt_text
